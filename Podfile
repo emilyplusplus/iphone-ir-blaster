405d64bb7c97865f540b7cc4aa5b1bbe7bb917ab
Emily Carlsen <emily.carlsen@redpillanalytics.com>
2017-03-09 16:46:37 -0500

    MVP

---
+++ Podfile
@@ -0,0 +1,28 @@
+project 'Remote.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Remote' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Remote
+
+  target 'RemoteTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RemoteUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+  
+  source 'https://github.com/CocoaPods/Specs.git'
+  platform :ios, '10.0'
+  use_frameworks!
+    
+  pod 'Alamofire', '~> 4.4'
+
+end
---
+++ Remote/VizioViewController.swift
@@ -0,0 +1,75 @@
+//
+//  VizioViewController.swift
+//  Remote
+//
+//  Created by Emily Carlsen on 3/7/17.
+//  Copyright © 2017 Emily Carlsen. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class VizioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func soundPower(_ sender: Any) {
+        Alamofire.request("http://10.0.1.11:3000/soundPower").responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+    }
+    
+    @IBAction func soundUp(_ sender: Any) {
+        Alamofire.request("http://10.0.1.11:3000/soundUp").responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+    }
+    
+    @IBAction func soundDown(_ sender: Any) {
+        Alamofire.request("http://10.0.1.11:3000/soundDown").responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
